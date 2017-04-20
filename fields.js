ef813f7afe654f76eaa93b36879af98065132c9e
Nadim Sayani <Nadim Sayani>
2017-04-20 16:46:43 -0700

    added fileds.js git commit -m added

---
+++ fields.js
@@ -0,0 +1,4 @@
+import StringWidget from './components/StringWidget';
+
+export const name = {name: 'name', displayName: 'Name', widget: StringWidget};
+export const age = {name: 'age', displayName: 'Age', widget: NumberWidget};
